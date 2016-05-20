856a5f571c5e9c558d5b1409634a5c871c27ed34
boween <boween@126.com>
2016-05-20 14:30:02 +0800

    增加板块内容

--- js/js.js
+++ js/js.js
@@ -11,7 +11,7 @@
 // console.log(window.btoa('(+86)18611605689'))
 function phonenumberencryption(){
 if (window.atob) { document.write( window.atob('KCs4NikxODYxMTYwNTY4OQ==') ); }
-if ( /[?&]show-phone-number=true(&|$)/.test(window.location.search) ) {
+if ( /[?&]show-phone-number(&|$)/.test(window.location.search) ) {
     document.getElementById('header__address__item-phone').style.display = 'block';
 }}
 //edulogo旋转效果及指针移入旋转
@@ -39,4 +39,17 @@ $(window).scroll(function () {
     if ($(window).scrollTop() == parseInt($(window).height()/200)*100) {
         rotateYDIV()
     }
+});
+$(document).ready(function(){
+    var t=$(".main_system").find(".item_list");
+    t.on("mouseenter", "a", function() {
+        var t = $(this).find("img");
+        t.attr("src", t.data("hover"));
+            $(this).find("span").addClass("span_active")
+    });
+        t.on("mouseleave", "a", function() {
+            var t = $(this).find("img");
+            t.attr("src", t.data("origin"));
+                $(this).find("span").removeClass("span_active")
+        });
 });
